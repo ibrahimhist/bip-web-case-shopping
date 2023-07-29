b7f6d5b7d3591f2bfe7ecccc5476b9d57cef44b0
ibrahimhist <hist.ibrahim@gmail.com>
2023-07-29 11:27:57 +0300

    showing image

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -7,8 +7,7 @@ export function createRandomProduct(): Product {
     id: faker.string.uuid(),
     price: parseInt(faker.commerce.price({ min: 1, max: 1000, dec: 0 })),
     name: faker.commerce.productName(),
-    // image: faker.image.url(),
-    image: '',
+    image: faker.image.url(),
     category: faker.commerce.department(),
     quantity: faker.number.int(10),
   };
